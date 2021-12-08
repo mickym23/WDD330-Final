7caa93417ac8e63b66a3fc2c56367dfd2cb85ee3
mickym23 <mikhail.cedras@gmail.com>
2021-12-08 17:50:48 +0200

    Added Home Page: Complete

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,18 @@
 exports.getIndex = (req, res, next) => {
+
+   const facts = [
+      'Did you know that a giraffe has a blue tongue?',
+      'An ostrich can run more than 40 miles in one hour.',
+      'Lions sleep for up to 20 hours every day.',
+      'Elephants don\'t have sweat glands, they use their trunks to spray themselves with water.',
+      'Leopard cubs are born blind, but their eyes start opening 10 days after birth.',
+      'Hippopotamuses can run faster than an average human.',
+      'Did you know that rhinoceros horns are made from keratin, the same material that human nails are made from.',
+      'Rhinoceroses communicate through infrasonic frequecies.'
+   ]
    res.render('home', {
-      pageTitle: 'Home | Kirafu Adventures'
+      pageTitle: 'Home | Kirafu Adventures',
+      facts: facts
    })
 }
 
